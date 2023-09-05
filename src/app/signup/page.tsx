580fcf1d1c7ff847f3c402dc3e57e30b9cdca37e
iamrajsingh <divyraj65407@gmail.com>
2023-09-06 00:02:54 +0530

    Basic Design Done

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,11 +1,57 @@
-"use client"; 
+"use client";
+import Link from "next/link";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+import { Axios } from "axios";
 
 export default function SignupPage() {
-    return (
-        <div className="flex">
-            <h1 className="text-center text-white text-2xl">
-                Signup screen
-            </h1>
-        </div>
-    )
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+    username: "",
+  });
+
+  const onSignup = async () => {};
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-center text-white text-2xl pb-8">Signup Here</h1>
+      <label htmlFor="username">username</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg m-4 focus:outline-none focus:border-gray-600 text-black"
+        type="text"
+        id="username"
+        value={user.username}
+        onChange={(e) => setUser({ ...user, username: e.target.value })}
+        placeholder="username"
+      />
+
+      <label htmlFor="email">email</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg m-4 focus:outline-none focus:border-gray-600 text-black"
+        type="email"
+        id="email"
+        value={user.email}
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+        placeholder="email"
+      />
+
+      <label htmlFor="password">password</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg m-4 focus:outline-none focus:border-gray-600 text-black"
+        type="password"
+        id="password"
+        value={user.password}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+        placeholder="password"
+      />
+
+
+      <button 
+      className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+      onClick={onSignup}
+      >Signup</button>
+      <Link href="/login">Already have an account</Link>
+    </div>
+  );
 }
