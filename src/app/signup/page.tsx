c98567fe223964db9242b073d3e1cd68972ddccb
iamrajsingh <divyraj65407@gmail.com>
2023-09-06 23:59:56 +0530

    Login and Signup done

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,21 +1,52 @@
 "use client";
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import { Axios } from "axios";
+import axios from "axios";
+import { toast } from "react-hot-toast";
 
 export default function SignupPage() {
+  const router = useRouter();
   const [user, setUser] = useState({
     email: "",
     password: "",
     username: "",
   });
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-  const onSignup = async () => {};
+  const onSignup = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post("api/users/signup", user);
+      toast.success("Account Created");
+      console.log("The response data is", response.data);
+      router.push("/login");
+    } catch (error: any) {
+      console.log("Signup failed", error.message);
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (
+      user.email.length > 0 &&
+      user.username.length > 0 &&
+      user.password.length > 0
+    ) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="text-center text-white text-2xl pb-8">Signup Here</h1>
+      <h1 className="text-center text-white text-2xl pb-8">
+        {loading ? "Loading..." : "Signup Here"}
+      </h1>
       <label htmlFor="username">username</label>
       <input
         className="p-2 border border-gray-300 rounded-lg m-4 focus:outline-none focus:border-gray-600 text-black"
@@ -46,11 +77,14 @@ export default function SignupPage() {
         placeholder="password"
       />
 
-
-      <button 
-      className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
-      onClick={onSignup}
-      >Signup</button>
+      {buttonDisabled ? <div className="p-2"><h1 className="text-xl">
+        Make button Visible by filling form
+        </h1></div> :<button
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        onClick={onSignup}
+      >
+        signup
+      </button>}
       <Link href="/login">Already have an account</Link>
     </div>
   );
