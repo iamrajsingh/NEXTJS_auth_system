bcb6493a5da075d3891bb1d2e00f08f51955016c
iamrajsingh <divyraj65407@gmail.com>
2023-09-08 00:56:35 +0530

    added middleware and me route

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,32 +1,56 @@
 "use client";
 
 import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
+export default function ProfilePage() {
+  const router = useRouter();
+  const [data, setData] = useState();
 
-export default function ProfilePage(){
-
-    const router = useRouter();
-
-    const logout = async ()=> {
-        try {
-            await axios.get('/api/users/logout')
-            router.push("/login");
-        } catch (error: any) {
-            console.log("Error while logout", error.message);
-
-        }
+  const logout = async () => {
+    try {
+      await axios.get("/api/users/logout");
+      router.push("/login");
+    } catch (error: any) {
+      console.log("Error while logout", error.message);
     }
+  };
 
-    return(
-        <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <h1>Profile</h1>
-            <hr/>
-            <p>Profile Page</p>
-            <hr />
-            <button 
-            onClick={logout}
-            className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none 
-            bg-red-600 focus:border-gray-600">logout</button>
-        </div>
-    )
+  const getUserDetails = async () => {
+    try {
+      const response = await axios.get("/api/users/me");
+      console.log(response.data);
+      setData(response.data.data._id);
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Profile</h1>
+      <hr />
+      <p>Profile Page</p>
+      <hr />
+      <h2 className="p-2 rounded-sm bg-slate-400 m-3">
+        {data ? <Link href={`/profile/${data}`}>{data}</Link> : "Data not here"}
+      </h2>
+      <button
+        onClick={logout}
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none 
+            bg-red-600 focus:border-gray-600"
+      >
+        logout
+      </button>
+      <hr />
+      <button
+        onClick={getUserDetails}
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none 
+            bg-blue-600 focus:border-gray-600"
+      >
+        Get User data
+      </button>
+    </div>
+  );
 }
