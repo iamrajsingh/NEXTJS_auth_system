d242e72e61634c04e4ca2fa833a844228c9821a5
iamrajsingh <divyraj65407@gmail.com>
2023-09-08 23:20:43 +0530

    Email Verification Done

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { NextResponse, NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
 
-  const isPublicPath = path === "/login" || path === "/signup";
+  const isPublicPath = path === "/login" || path === "/signup" || "/verifyemail";
 
   const token = request.cookies.get("token")?.value || "";
 
@@ -18,5 +18,5 @@ export function middleware(request: NextRequest) {
 
 // Matching paths
 export const config = {
-  matcher: ["/", "/login", "/signup", "/profile"],
+  matcher: ["/", "/login", "/signup", "/profile", "/verifyemail"],
 };
